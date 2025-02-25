beab3ab21119311aaa425f85a97b0dae0a69ae00
ASWINKUMAR26-IT <157028907+ASWINKUMAR26-IT@users.noreply.github.com>
2025-02-25 22:43:50 +0530

    Add files via upload

---
+++ practils.py
@@ -0,0 +1,88 @@
+# n=int(input("Enter the number of terms::"))
+# a,b=0,1
+# for i in range(n):
+#   print(a," ")
+#   a,b=b,a+b
+
+# lst=[0,0,0]
+
+# for i in range(len(lst)):
+#   print("lst =",[lst[i]])
+# print("Before insertion of array...")
+# for i in range (len(lst)):
+#   lst[i]=i+2
+# # print("After insertion of array...")
+# # for i in range (len(lst)):
+# #   print("lst =",[lst[i]])
+# print(lst)
+# print(len(lst)-1)
+
+# arr=[78,67,34,90,75]
+# # print(arr)
+# arr.sort()
+# print(arr)
+
+# Stack implementation using a list
+# stack=[]
+# def display(n):
+#   print(n)
+
+# stack.append(10)
+# stack.append(20)
+# stack.append(30)
+# display(stack)
+#--------------------------------------------------------------------
+
+# list = [ 'abcd', 786, 2.23, 'john', 70.2] 
+# print (list[2:])
+
+# sum = 2 + 3 
+# print(sum) 
+# s = '2' + '3' 
+# print(s) 
+
+# A=[10,20,30,40,50] 
+# b=[11,12,13,14,15]
+# A.extend(b)
+# print(A)
+
+# tp=() 
+# tp1=tp*2 
+# print(len(tp1))
+
+# st=range(5)
+# print(st)
+
+# num=0
+# for i in range(1,100):
+#   num+=i
+# print(num)
+
+
+# def is_adam_number(num):
+#     # Step 1: Find the square of the number
+#     square_num = num ** 2
+#     print(square_num)
+#     # Step 2: Reverse the number
+#     reversed_num = int(str(num)[::-1])
+#     print(reversed_num)
+#     # Step 3: Find the square of the reversed number
+#     square_reversed_num = reversed_num ** 2
+#     print(square_reversed_num)
+#     # Step 4: Reverse the squared reversed number
+#     reversed_square = int(str(square_reversed_num)[::-1])
+#     print(reversed_square)
+#     print(square_num == reversed_square)
+#     # Step 5: Check if the original square matches the reversed square
+#     return square_num == reversed_square
+
+# # Check for 12
+# number = 12
+# if is_adam_number(number):
+#     print(f"{number} is an Adam Number.")
+# else:
+#     print(f"{number} is not an Adam Number.")
+
+
+
+
